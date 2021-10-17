28aeef5ad4adde1c5a4595a529369a224d04a9a8
marian1989 <marianjavorsky89@gmail.com>
2021-10-17 19:02:33 +0200

    changed name

--- app/src/main/java/com/example/spacex/ui/missions/MissionsFragment.kt
+++ app/src/main/java/com/example/spacex/ui/missions/MissionsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.spacex.ui.home
+package com.example.spacex.ui.missions
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.spacex.R
 
-class HomeFragment : Fragment() {
+class MissionsFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var missionsViewModel: MissionsViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        missionsViewModel =
+                ViewModelProvider(this).get(MissionsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_missions, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        missionsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/spacex/ui/missions/MissionsViewModel.kt
+++ app/src/main/java/com/example/spacex/ui/missions/MissionsViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.spacex.ui.home
+package com.example.spacex.ui.missions
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class MissionsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
